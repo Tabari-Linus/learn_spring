e471af610d82b0acf59b75b016f6fa426c6e32b8
Mr Lii <leetamparega@gmail.com>
2025-05-23 23:54:42 +0000

    adjustment to data files

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,8 +4,8 @@ DROP SEQUENCE IF EXISTS widgets_id_seq;
 CREATE SEQUENCE widgets_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1;
 
 CREATE TABLE "widgets" (
-                           "id" bigint DEFAULT nextval('widgets_id_seq') NOT NULL,
-                           "name" text,
-                           "purpose" text,
-                           CONSTRAINT "widgets_pkey" PRIMARY KEY ("id")
+"id" bigint DEFAULT nextval('widgets_id_seq') NOT NULL,
+"name" text,
+"purpose" text,
+CONSTRAINT "widgets_pkey" PRIMARY KEY ("id")
 );
