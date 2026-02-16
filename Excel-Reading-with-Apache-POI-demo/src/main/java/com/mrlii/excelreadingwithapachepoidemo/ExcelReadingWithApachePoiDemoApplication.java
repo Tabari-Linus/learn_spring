47e03031ab12855f87dc863be95cf6f91f23d3ed
mr_lii <leetamparega@gmail.com>
2026-02-16 07:43:42 +0000

    docs: working with excel

---
+++ Excel-Reading-with-Apache-POI-demo/src/main/java/com/mrlii/excelreadingwithapachepoidemo/ExcelReadingWithApachePoiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mrlii.excelreadingwithapachepoidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelReadingWithApachePoiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelReadingWithApachePoiDemoApplication.class, args);
+    }
+
+}
---
+++ Excel-Reading-with-Apache-POI-demo/src/test/java/com/mrlii/excelreadingwithapachepoidemo/ExcelReadingWithApachePoiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlii.excelreadingwithapachepoidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelReadingWithApachePoiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
