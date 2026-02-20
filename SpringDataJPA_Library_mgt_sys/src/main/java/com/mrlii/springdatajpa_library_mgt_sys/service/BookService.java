9a2aa1c4b8ab052776ef79a1e3711fb7b339f6f1
mr_lii <leetamparega@gmail.com>
2026-02-20 10:43:08 +0000

    feat: add get book by id functionality
    - Create `BookResponse` DTO record to hold book details and author name.
    - Add `getBookById` method in `BookService` to retrieve a book by ID and map it to `BookResponse`.

--- SpringDataJPA_Library_mgt_sys/src/main/java/com/mrlii/springdatajpa_library_mgt_sys/service/BookService.java
+++ SpringDataJPA_Library_mgt_sys/src/main/java/com/mrlii/springdatajpa_library_mgt_sys/service/BookService.java
@@ -1,17 +1,18 @@
 package com.mrlii.springdatajpa_library_mgt_sys.service;
 
+import com.mrlii.springdatajpa_library_mgt_sys.dto.BookResponse;
 import com.mrlii.springdatajpa_library_mgt_sys.entity.Book;
 import com.mrlii.springdatajpa_library_mgt_sys.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
-
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     public Book createBook(String title, String isbn) {
         Book newBook = new Book();
@@ -19,5 +20,15 @@ public class BookService {
         newBook.setIsbn(isbn);
 
         return bookRepository.save(newBook);
+    }
+
+    public BookResponse getBookById(Long id){
+        Optional<Book> book = bookRepository.findById(id);
+        return book.map(value -> new BookResponse(
+                value.getId(),
+                value.getTitle(),
+                value.getIsbn(),
+                value.getAuthor().getName()
+        )).orElse(null);
     }
 }
