22decdc378de9c391d08a42de6b7ff74ebc08d8b
mr_lii <leetamparega@gmail.com>
2026-02-23 11:26:07 +0000

    feat: add validation using Jakarta Validation and update DTOs

--- SpringDataJPA_Library_mgt_sys/src/main/java/com/mrlii/springdatajpa_library_mgt_sys/controller/AuthorController.java
+++ SpringDataJPA_Library_mgt_sys/src/main/java/com/mrlii/springdatajpa_library_mgt_sys/controller/AuthorController.java
@@ -4,6 +4,7 @@ import com.mrlii.springdatajpa_library_mgt_sys.dto.request.CreateAuthorRequest;
 import com.mrlii.springdatajpa_library_mgt_sys.dto.response.ApiResponse;
 import com.mrlii.springdatajpa_library_mgt_sys.dto.response.AuthorResponse;
 import com.mrlii.springdatajpa_library_mgt_sys.service.AuthorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class AuthorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<ApiResponse<AuthorResponse>> createAuthor(@RequestBody CreateAuthorRequest request){
+    public ResponseEntity<ApiResponse<AuthorResponse>> createAuthor(@Valid @RequestBody CreateAuthorRequest request){
         ApiResponse<AuthorResponse> response = authorService.createAuthor(request);
         return ResponseEntity.ok(response);
     }
--- SpringDataJPA_Library_mgt_sys/src/main/java/com/mrlii/springdatajpa_library_mgt_sys/dto/request/CreateAuthorRequest.java
+++ SpringDataJPA_Library_mgt_sys/src/main/java/com/mrlii/springdatajpa_library_mgt_sys/dto/request/CreateAuthorRequest.java
@@ -1,7 +1,14 @@
 package com.mrlii.springdatajpa_library_mgt_sys.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateAuthorRequest(
+        @NotBlank(message = "Author name cannot be blank")
+        @Size(min = 2, max = 100, message = "Author name must be between 3 and 100 characters")
         String name,
+
+        @NotBlank(message = "Author nationality cannot be blank")
         String nationality
 ) {
 }
