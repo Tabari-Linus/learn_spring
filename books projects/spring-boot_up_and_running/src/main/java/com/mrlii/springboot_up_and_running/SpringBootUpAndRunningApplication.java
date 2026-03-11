ece2f17882d401eb763cebbd114f77d65c0158d6
Linus Tabari <leetamparega@gmail.com>
2026-03-11 16:45:59 +0000

    docs: REST API

--- books projects/spring-boot_up_and_running/src/main/java/com/mrlii/springboot_up_and_running/SpringBootUpAndRunningApplication.java
+++ books projects/spring-boot_up_and_running/src/main/java/com/mrlii/springboot_up_and_running/SpringBootUpAndRunningApplication.java
@@ -2,6 +2,12 @@ package com.mrlii.springboot_up_and_running;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootUpAndRunningApplication {
@@ -10,4 +16,46 @@ public class SpringBootUpAndRunningApplication {
         SpringApplication.run(SpringBootUpAndRunningApplication.class, args);
     }
 
+}
+
+
+class Coffee{
+    private  final String id;
+    private String name;
+
+    public Coffee(String id, String name){
+        this.id = id;
+        this.name =name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
+
+@RestController
+class RestDemoController{
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestDemoController(){
+        coffees.addAll(List.of(
+                new Coffee("1", "Latte"),
+                new Coffee("2", "Americano"),
+                new Coffee("3", "Mocha")
+        ));
+
+    }
+
+    @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    Iterable<Coffee> getCoffees() {
+        return coffees;
+    }
 }
