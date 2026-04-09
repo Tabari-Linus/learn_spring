6d0ff18c1a68ea4994088a65e86946544894b8c4
Linus Tabari <leetamparega@gmail.com>
2026-04-09 10:47:36 +0000

    feat: initialize Spring Boot task management application with Maven wrapper, JPA, H2, and web dependencies

---
+++ TDD-TaskManagementApplication/src/main/java/com/mrlii/tddtaskmanagementapplication/TddTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mrlii.tddtaskmanagementapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddTaskManagementApplication.class, args);
+	}
+
+}
---
+++ TDD-TaskManagementApplication/src/test/java/com/mrlii/tddtaskmanagementapplication/TddTaskManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mrlii.tddtaskmanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// nothing
+	}
+
+}
